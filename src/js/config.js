02dd28c6fc202809752fb87ae7923c9ea2a85e5e
TayyabAli491 <alihaniff5@gmail.com>
2024-03-22 23:18:04 +0500

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '431b80dd-7553-4901-82a6-bcbf6ddf6829';
+export const MODAL_CLOSE_SEC = 2.5;
